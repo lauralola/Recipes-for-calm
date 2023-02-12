b4b39f9d182a992252c0d2fc6072dc86d8abcc2f
lauralola <laura6289@gmail.com>
2023-02-12 19:01:53 +0000

    Add like functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from .models import Recipe
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
 
 class RecipeList(generic.ListView):
     model = Recipe
@@ -13,7 +14,7 @@ class RecipeDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
@@ -61,3 +62,13 @@ class RecipeDetail(View):
                 "liked": liked
             },
         )
+
+class RecipeLike(View):
+    def recipe(self, request, slug):
+        recipe = get_object_or_404(Recipe, slug=slug)
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.likes.remove(request.user)
+        else:
+            recipe.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
