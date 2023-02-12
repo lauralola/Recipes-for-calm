88f896aa195f8b31695050222ce74930edd20196
lauralola <laura6289@gmail.com>
2023-02-12 19:13:22 +0000

    Add messaging functionality

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from .models import Recipe
 from .forms import CommentForm
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 class RecipeList(generic.ListView):
     model = Recipe
@@ -34,7 +35,7 @@ class RecipeDetail(View):
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
@@ -46,16 +47,16 @@ class RecipeDetail(View):
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.recipe = recipe
             comment.save()
         else:
             comment_form = CommentForm()
 
         return render(
             request,
-            "post_detail.html",
+            "recipe_detail.html",
             {
-                "post": post,
+                "recipe": recipe,
                 "comments": comments,
                 "commented": True,
                 "comment_form": comment_form,
@@ -70,5 +71,6 @@ class RecipeLike(View):
             recipe.likes.remove(request.user)
         else:
             recipe.likes.add(request.user)
+            messages.add_message(request, messages.INFO, 'Thank you for your comment!')
         
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
